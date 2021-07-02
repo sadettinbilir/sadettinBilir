ef55a387b746ecf012e8582eefd8995c62eb41a2
sadettinbilir <sadettinbilir@gmail.com>
2021-07-02 12:10:25 +0000

    kimlik anahtar değeri

--- sadettin.py
+++ sadettin.py
@@ -2,5 +2,6 @@ import json
 dosya=open('myfile.json',"r")
 json_dosya=json.load(dosya)
 print("devnet ödev1")
+print("API-KEY :",json_dosya["access_token"])
 print("Kullanıcı adı :",json_dosya["kimlik"][0]["ad"])
 print("Kullanıcı soyadı :",json_dosya["kimlik"][1]["soyad"])
